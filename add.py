b0c684b6ec2b0479a1c68c04ebb6a626f1a20ddc
Pranav-2128 <pikarai38@gmail.com>
2021-05-23 15:05:13 +0530

    another file

--- add.py
+++ add.py
@@ -1,4 +1,12 @@
 x = int(input('Enter the value of x: '))
+
+
+a =  x + y
+b =  x - y
+
+print("a is = ",a)
+print("b is = ",b)
+x = int(input('Enter the value of x: '))
 y = int(input('Enter the value of y: '))
 
 a =  x + y
