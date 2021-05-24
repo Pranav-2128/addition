41e1b17bcd90568c9415509bd0e508f6779cd66c
Pranav-2128 <pikarai38@gmail.com>
2021-05-24 10:24:55 +0530

    new operator

--- add.py
+++ add.py
@@ -1,12 +1,4 @@
 x = int(input('Enter the value of x: '))
-
-
-a =  x + y
-b =  x - y
-
-print("a is = ",a)
-print("b is = ",b)
-x = int(input('Enter the value of x: '))
 y = int(input('Enter the value of y: '))
 
 a =  x + y
@@ -14,3 +6,6 @@ b =  x - y
 
 print("a is = ",a)
 print("b is = ",b)
+
+c = x * y
+print(c)
